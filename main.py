12bc44619de5cc0480d85e73f3e8d2b043904848
Prachaya Praphaphan <pgr88o00@gmail.com>
2024-07-28 02:13:34 +0700

    last assignment

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 children = [
-    {"name": "Alice","age" : 2, "height" : 95},
-    {"name": "Bob","age" : 4, "height" : 105},
-    {"name": "Charlie","age" : 3, "height" : 110},
-    {"name": "David","age" : 5, "height" : 102},
-    {"name": "Eve","age" : 6, "height" : 99}
+    {"name": "Alice", "age" : 2, "height" : 95},
+    {"name": "Bob", "age" : 4, "height" : 105},
+    {"name": "Charlie", "age" : 3, "height" : 110},
+    {"name": "David", "age" : 5, "height" : 102},
+    {"name": "Eve", "age" : 6, "height" : 99}
 ]
 
-eligible_children = [child for child in children if child['age'] > 3 and child['height'] > 100]
+criteria = lambda child: child['age'] > 3 and child['height'] > 100
+
+eligible_children = [child for child in children if criteria(child)]
 
 print("Eligible children for the fun park:", eligible_children)
