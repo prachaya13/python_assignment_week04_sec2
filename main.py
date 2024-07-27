d7b5e3d355616c8be9f77d27a901b57a34f57cc3
Prachaya Praphaphan <pgr88o00@gmail.com>
2024-07-28 01:45:20 +0700

    lambda_calculator

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 x = int(input("Enter the value of x: "))
 y = int(input("Enter the value of y: "))
 
-import calculator
-print(f"X+Y = {calculator.add(x, y)}")
-print(f"X-Y = {calculator.subtract(x, y)}")
-print(f"X*Y = {calculator.multiply(x, y)}")
-print(f"X/Y = {calculator.divide(x, y)}")
+import lambda_calculator
+print(f"X+Y = {lambda_calculator.add(x, y)}")
+print(f"X-Y = {lambda_calculator.subtract(x, y)}")
+print(f"X*Y = {lambda_calculator.multiply(x, y)}")
+print(f"X/Y = {lambda_calculator.divide(x, y)}")
